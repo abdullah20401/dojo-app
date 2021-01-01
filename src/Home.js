e026f16ab199b502313139c5d9f141deb7fddeec
Abdullah Siddiqui <siddiquiabdullah2017@gmail.com>
2021-01-01 06:15:50 +0000

    #7 - Click Events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('Hello, Users!', e);
+    }
+
+    const HandleClickAgain = (name, e) => {
+        console.log('Hello, ' + name, e.target);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me!</button>
+            <button onClick={(e) => HandleClickAgain('Mario', e)}>Click Me Again!</button>
         </div>
     );
 }
